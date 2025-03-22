b67d00091d4daa158dd791e52452cb810f88031b
Yossia Dwi Mahardika <yossia.dwi@ti.ukdw.ac.id>
2025-03-22 14:08:15 +0700

    Initial commit

---
+++ src/main/java/com/example/belajar_spring_monitoring/BelajarSpringMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/belajar_spring_monitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
